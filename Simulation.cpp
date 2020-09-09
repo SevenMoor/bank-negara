cfead89b87085f9cc49ffbee8b611fd4b3fe4111
SevenMoor <au.ottaviano@yahoo.com>
2020-09-09 16:33:01 +0200

    Added created events to the SED

--- Simulation.cpp
+++ Simulation.cpp
@@ -7,6 +7,7 @@
 Simulation::Simulation(double expectedDuration, double expectedServiceTime, double interval, int cashierCount) : expectedDuration(expectedDuration), expectedServiceTime(expectedServiceTime), interval(interval), cashierCount(cashierCount), SED(), bank(Bank(cashierCount,expectedServiceTime,this)) {
    double firstTime = interval + exp(-1)*exp(random());
    Arrival first = Arrival(firstTime,this);
+   this->add(first);
 }
 
 double Simulation::getInterval(){
