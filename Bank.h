49131cd45f12a2bc6668feaa473b49d5c281e786
SevenMoor <au.ottaviano@yahoo.com>
2020-09-09 13:45:50 +0200

    Added Bank and cashier headers
    Signed-off-by: SevenMoor <au.ottaviano@yahoo.com>

--- Bank.h
+++ Bank.h
@@ -1,6 +1,17 @@
 #ifndef BANK_H
 #define BANK_H
 
+#include "Cashier.h"
 
+class Bank{
+    private:
+        int cashierCount;
+        int clientCount;
+        Cashier* cashiers;
+    public:
+        int getCashierCount();
+        int getClientCount();
+        Cashier freeCashier();
+};
 
 #endif
--- Cashier.h
+++ Cashier.h
@@ -1,6 +1,20 @@
 #ifndef CASHIER_H
 #define CASHIER_H
 
-
+class Cashier{
+    private:
+        double occupationRate;
+        int clientCount;
+        double averageServiceTime;
+        bool available;
+        Client client;
+    public:
+        double getOccupationRate();
+        int getClientCount();
+        double getAverageServiceTime();
+        bool isAvailable();
+        void serve(Client client);
+        void free();
+};
 
 #endif
