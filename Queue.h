afb15786459f3b13fbb19c6b986d6d2b90aa15b2
SevenMoor <au.ottaviano@yahoo.com>
2020-09-09 13:57:18 +0200

    Added Queue headers

--- Queue.h
+++ Queue.h
@@ -1,6 +1,22 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <deque>
 
+using namespace std;
+
+class Queue{
+    private:
+        deque<Client> clients;
+        int maxLength;
+        double averageLength;
+        bool empty;
+    public:
+        int getMaxLength();
+        double getAverageLength();
+        bool isEmpty();
+        void add(Client client);
+        Client pop();
+};
 
 #endif
