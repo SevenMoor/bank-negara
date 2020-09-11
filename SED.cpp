2bf0d8a9459621815c136818beee21db6d296957
SevenMoor <au.ottaviano@yahoo.com>
2020-09-11 11:09:13 +0200

    Added SED::provideWaitEntry method

--- SED.cpp
+++ SED.cpp
@@ -1,6 +1,6 @@
 #include "SED.h"
 
-SED::SED(double initTime) : initTime(initTime){
+SED::SED(double initTime) : initTime(initTime), totalWaitTime(0) {
     this->schedule = priority_queue<Event,vector<Event>,EventComparator>();
 }
 
@@ -17,6 +17,19 @@ void SED::start(){
 
         schedule.pop();
     }
+
+    //STATISTICS
+    //------------
+    //Real duration --> ici
+    //Queue length (max+avg) --> bank.queue
+    //Client count --> bank.cashiers
+    //Occupation rate --> bank.cashiers
+    //Client avg waitTime --> ???
+
+}
+
+void SED::provideWaitTimeEntry(double wait){
+    totalWaitTime += wait;
 }
 
 double SED::getCurrentTime(){
