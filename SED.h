29b3394232cdd02ecd2ff971f7e35307dc8ec1cd
SevenMoor <au.ottaviano@yahoo.com>
2020-09-18 19:49:09 +0200

    Documented SED

--- SED.h
+++ SED.h
@@ -1,3 +1,16 @@
+/**
+ * 
+ * @file SED.h
+ * @author Aurélien OTTAVIANO
+ * @date 10/09/2020
+ * 
+ * @class SED
+ * @brief An event management system for the simulation
+ * 
+ * It uses a priority_queue to register events, and order them according to the time of the event in ascending order. Chosen events are then processed.
+ * 
+ */ 
+
 #ifndef SED_H
 #define SED_H
 
@@ -6,17 +19,56 @@
 
 #include "Event.h"
 
+using namespace std;
+
 class SED{
 	protected:
-		double currentTime;
-		double totalWaitTime;
-		std::priority_queue<Event*, std::vector<Event*>, EventComparator> schedule;
+		double currentTime; ///< The current time in the simulation
+		double totalWaitTime; ///< The total time waited by all clients that have been in the queue
+		priority_queue<Event*, vector<Event*>, EventComparator> schedule; ///< The priority queue used for storing and ordering events
 
 	public:
+		/**
+		 * 
+		 * @fn SED()
+		 * @brief The constructor for the SED class.
+		 * 
+		 */
 		SED();
+
+		/**
+		 * 
+		 * @fn void start()
+		 * @brief The main loop of the event processing simulation.
+		 * 
+		 */
 		void start();
+
+		/**
+		 * 
+		 * @fn void add(Event* e)
+		 * @brief Method used to add a new event to the scheduler.
+		 * @param e pointer to the Event to add to the scheduler 
+		 * 
+		 */
 		void add(Event *e);
+
+		/**
+		 * 
+		 * @fn double getCurrentTime() const
+		 * @brief Read accessor for the current time of the simulation.
+		 * @return The current time of the simulation
+		 * 
+		 */
 		double getCurrentTime() const;
+
+		/**
+		 * 
+		 * @fn void provideWaitTimeEntry(doublr time)
+		 * @brief Adds an amount of wait time to the total waited time.
+		 * @param time The amount of time to add 
+		 * 
+		 */
 		void provideWaitTimeEntry(double time);
 };
 
